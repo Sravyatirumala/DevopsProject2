44e85d8a460f6b79cca06185f63fcd6d4b6f0556
Sravyatirumala <104963297+Sravyatirumala@users.noreply.github.com>
2025-04-08 16:32:22 +0530

    Update Sample_contentCarousel.jsx

--- src/components/Sample_contentCarousel.jsx
+++ src/components/Sample_contentCarousel.jsx
@@ -1,118 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import ContentTile from "./ContentTile";
-import ContentTileData from "./ContentTileData";
-
-function abc(data) {
-  return <ContentTile poster={data.poster} />;
-}
-
-function ContentCarousel() {
-  return (
-    <div style={{ marginBottom: 48 }}>
-      <div style={{ marginLeft: 72, marginRight: 72, paddingBottom: 16 }}>
-        Movies List
-      </div>
-      <div>
-        <div style={{ display: "flex", overflow: "hidden" }}>
-          <button
-            onClick={() => {
-              document.getElementById("container").scrollLeft -= 1400;
-            }}
-            style={{
-              backgroundColor: "transparent",
-              zIndex: 20,
-              // display: "none",
-            }}
-          >
-            left
-          </button>
-          <ul id="container" style={{ zIndex: 2 }}>
-          import React from "react";
-import ReactDOM from "react-dom/client";
-import ContentTile from "./ContentTile";
-import ContentTileData from "./ContentTileData";
-
-function abc(data) {
-  return <ContentTile poster={data.poster} />;
-}
-
-function ContentCarousel() {
-  return (
-    <div style={{ marginBottom: 48 }}>
-      <div style={{ marginLeft: 72, marginRight: 72, paddingBottom: 16 }}>
-        Movies List
-      </div>
-      <div>
-        <div style={{ display: "flex", overflow: "hidden" }}>
-          <button
-            onClick={() => {
-              document.getElementById("container").scrollLeft -= 1400;
-            }}
-            style={{
-              backgroundColor: "transparent",
-              zIndex: 20,
-              // display: "none",
-            }}
-          >
-            left
-          </button>
-          <ul
-            id="container"
-            style={{
-              alignItems: "flex-start",
-              display: "flex",
-              flexDirection: "row",
-              scrollSnapType: "x proximity",
-              listStyle: "none",
-              overflowX: "scroll",
-              scrollBehavior: "smooth",
-              padding: "72 72",
-              margin: 0,
-              zIndex: 2,
-            }}
-          >
-            {ContentTileData.map(abc)}
-          </ul>
-          <button
-            onClick={() => {
-              document.getElementById("container").scrollLeft += 1400;
-            }}
-            style={{
-              backgroundColor: "transparent",
-              zIndex: 20,
-              border: "none",
-              appearance: "none",
-              // display: "none",
-            }}
-          >
-            <img
-              src="https://www.citypng.com/public/uploads/preview/right-arrowhead-black-11581593809dmlihnv7fa.png"
-              alt="right"
-              style={{ width: 20, height: 20 }}
-            ></img>
-          </button>
-        </div>
-        {/* <button
-          onClick={() => {
-            document.getElementById("container").scrollLeft += 1400;
-          }}
-        >
-          ABC
-        </button> */}
-      </div>
-    </div>
-  );
-}
-
-export default ContentCarousel;
-
-
-**new**
-
-
-import React from "react";
-import ReactDOM from "react-dom/client";
 import ContentTile from "./ContentTile";
 import ContentTileData from "./ContentTileData";
 
@@ -130,118 +16,71 @@ function ContentCarousel() {
           paddingBottom: 16,
           alignItems: "center",
           display: "flex",
-          flexDirection: "row",
-          position: "relative",
         }}
       >
-        <h2
-          style={{
-            alignItems: "center",
-            display: "flex",
-            flexDirection: "row",
-          }}
-        >
-          Movies List
-        </h2>
+        <h2 style={{ margin: 0 }}>Movies List</h2>
       </div>
-      <div>
-        <div
+
+      <div
+        style={{
+          position: "relative",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        {/* Left Scroll Button */}
+        <button
+          onClick={() => {
+            document.getElementById("container").scrollLeft -= 1400;
+          }}
           style={{
-            display: "grid",
-            gridTemplateAreas: "left right",
-            gridTemplateColumns: "repeat(2,48)",
-            gridTemplateRows: "1fr",
-            justifyContent: "space-between",
-            overflow: "hidden",
-            minWidth: 320,
+            position: "absolute",
+            left: 0,
+            zIndex: 10,
+            backgroundColor: "transparent",
+            border: "none",
+            cursor: "pointer",
+            height: "100%",
+            padding: "0 10px",
           }}
         >
-          <button
-            onClick={() => {
-              document.getElementById("container").scrollLeft -= 1400;
-            }}
-            style={{
-              gridArea: "left",
-              // gridColumnStart: "left",
-              // gridColumnEnd: "left",
-              // gridRowStart: 1,
-              // gridRowEnd: "auto",
-              alignItems: "center",
-              cursor: "pointer",
-              display: "grid",
-              justifyItems: "center",
-              margin: 0,
-              maxWidth: 30,
-              backgroundColor: "transparent",
-              zIndex: 20,
-              position: "relative",
-              // display: "none",
-            }}
-          >
-            left
-          </button>
-          <ul
-            id="container"
-            style={{
-              gridColumnStart: "left-start",
-              gridColumnEnd: "right-end",
-              gridRowStart: 1,
-              gridRowEnd: "auto",
-              zIndex: 1,
-              alignItems: "flex-start",
-              display: "flex",
-              flexDirection: "row",
-              scrollSnapType: "x proximity",
-              listStyle: "none",
-              overflowX: "scroll",
-              scrollBehavior: "smooth",
-              padding: "72 72",
-              margin: 0,
-            }}
-          >
-            {ContentTileData.map(abc)}
-          </ul>
-          <button
-            onClick={() => {
-              document.getElementById("container").scrollLeft += 1400;
-            }}
-            style={{
-              gridArea: "right",
-              // gridColumnStart: "right",
-              // gridColumnEnd: "right",
-              // gridColumn: "right",
-              // gridRowStart: "1",
-              // gridRowEnd: "1",
-              alignItems: "center",
-              cursor: "pointer",
-              display: "grid",
-              justifyItems: "center",
-              margin: 0,
-              maxWidth: 30,
-              padding: 0,
-              backgroundColor: "transparent",
-              zIndex: 20,
-              position: "relative",
-              // border: "none",
-              // appearance: "none",
-              // display: "none",
-            }}
-          >
-            right
-            {/* <img
-              src="https://www.citypng.com/public/uploads/preview/right-arrowhead-black-11581593809dmlihnv7fa.png"
-              alt="right"
-              style={{ width: 20, height: 20 }}
-            ></img> */}
-          </button>
-        </div>
-        {/* <button
+          &#8592;
+        </button>
+
+        {/* Carousel Container */}
+        <ul
+          id="container"
+          style={{
+            listStyle: "none",
+            display: "flex",
+            overflowX: "auto",
+            scrollSnapType: "x mandatory",
+            scrollBehavior: "smooth",
+            padding: "0 72px",
+            margin: 0,
+          }}
+        >
+          {ContentTileData.map(abc)}
+        </ul>
+
+        {/* Right Scroll Button */}
+        <button
           onClick={() => {
             document.getElementById("container").scrollLeft += 1400;
           }}
+          style={{
+            position: "absolute",
+            right: 0,
+            zIndex: 10,
+            backgroundColor: "transparent",
+            border: "none",
+            cursor: "pointer",
+            height: "100%",
+            padding: "0 10px",
+          }}
         >
-          ABC
-        </button> */}
+          &#8594;
+        </button>
       </div>
     </div>
   );
